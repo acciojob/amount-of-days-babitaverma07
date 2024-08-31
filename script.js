053d7390d0f24c1a0d0a9d3d2c375f9e405bbb91
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 22:23:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if (year % 4 === 0) { // check if year is a leap year
+    if (year % 100 === 0) { // check if year is a century year
+      if (year % 400 === 0) { // check if year is a leap century year
+        return 366; // return 366 for leap century years
+      } else {
+        return 365; // return 365 for non-leap century years
+      }
+    } else {
+      return 366; // return 366 for leap years
+    }
+  } else {
+    return 365; // return 365 for non-leap years
+  }
+}
